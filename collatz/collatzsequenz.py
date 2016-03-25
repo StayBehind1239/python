952730c2e97cd82bd2a3160a7590d3bb9d07da89
StayBehind1239 <yanneck.robo@gmail.com>
2016-03-25 16:37:30 +0100

    collatz sequenz

---
+++ collatz/collatzsequenz.py
@@ -0,0 +1,19 @@
+def collatz(number):
+
+        while number != 1:
+                
+                if number % 2 == 0:
+                        number = number // 2
+                        print(number)
+
+                elif number % 2 == 1:
+                        number = 3 * number + 1
+                        print(number)
+                        
+
+try:
+        print('the simplest impossible math problem... give me number and i will show you ^^')
+        newnumber = int(input())
+        collatz(newnumber)
+except ValueError:
+        print('Maybe you should use a integer ^^')
